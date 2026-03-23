9f127ab41e48a1ae4e2950c6ad5e3d5e0fb9007b
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-03-23 19:04:00 +0100

    v0.1

---
+++ 1.6/Source/DefOfs/InternalDefOf.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+namespace VanillaQuestsExpandedDroneFactory
+{
+	[DefOf]
+	public static class InternalDefOf
+	{
+		static InternalDefOf()
+		{
+			DefOfHelper.EnsureInitializedInCtor(typeof(InternalDefOf));
+		}
+
+		public static JobDef VQE_GotoPatrolDest;
+
+
+    }
+}
