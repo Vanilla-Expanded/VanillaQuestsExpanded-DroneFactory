784636c9cac6d5a9d63bba07e1c220412ccc8e40
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-03-24 19:37:30 +0100

    Unfinished drone spawner

---
+++ 1.6/Source/Comps/Properties/CompProperties_DroneHatcher.cs
@@ -0,0 +1,18 @@
+﻿
+using Verse;
+
+namespace VanillaQuestsExpandedDroneFactory
+{
+    public class CompProperties_DroneHatcher : CompProperties
+    {
+
+        public PawnKindDef hatcherKindDef;
+       
+        public CompProperties_DroneHatcher()
+        {
+
+            this.compClass = typeof(CompDroneHatcher);
+        }
+    }
+}
+
