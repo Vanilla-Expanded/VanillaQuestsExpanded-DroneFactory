cfbf724dae3cfc1d0c4735b5174ea0633509adfd
Taranchuk <nfhfyxer@gmail.com>
2026-03-25 20:13:48 +0200

    fixes

--- 1.6/Source/AI/MentalStates/MentalState_DiagnosticLoop.cs
+++ 1.6/Source/AI/MentalStates/MentalState_DiagnosticLoop.cs
@@ -5,10 +5,15 @@ namespace VanillaQuestsExpandedDroneFactory
 {
     public class MentalState_DiagnosticLoop : MentalState
     {
+        private int recoveryTicks;
+
         public override void PostStart(string reason)
         {
             base.PostStart(reason);
-            pawn.stances.stunner.StunFor(int.MaxValue, null, showMote: true);
+            pawn.jobs.StopAll();
+            pawn.pather.StopDead();
+            recoveryTicks = Rand.RangeInclusive(def.minTicksBeforeRecovery, def.maxTicksBeforeRecovery);
+            pawn.stances.stunner.StunFor(recoveryTicks, null, showMote: true);
         }
 
         public override void PostEnd()
@@ -20,10 +25,21 @@ namespace VanillaQuestsExpandedDroneFactory
         public override void MentalStateTick(int delta)
         {
             base.MentalStateTick(delta);
+            if (age >= recoveryTicks)
+            {
+                RecoverFromState();
+                return;
+            }
             if (pawn.IsHashIntervalTick(60))
             {
                 pawn.Rotation = new Rot4((pawn.Rotation.AsInt + 1) % 4);
             }
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref recoveryTicks, "recoveryTicks");
         }
     }
 }
