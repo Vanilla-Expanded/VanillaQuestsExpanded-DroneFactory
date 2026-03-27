bc0f98bed3311b5c9b9265d00979f51d54af2084
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-03-27 16:29:09 +0100

    Drone fiesta

--- 1.6/Source/Comps/CompProximityFuseWithCount.cs
+++ 1.6/Source/Comps/CompProximityFuseWithCount.cs
@@ -1,62 +1,63 @@
-﻿
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using RimWorld;
-using VanillaQuestsExpandedDroneFactory;
 using Verse;
-using Verse.AI;
-using Verse.Noise;
 
-public class CompProximityFuseWithCount : ThingComp
+namespace VanillaQuestsExpandedDroneFactory
+
 {
-    public CompProperties_ProximityFuseWithCount Props => (CompProperties_ProximityFuseWithCount)props;
-
-    public override void CompTick()
+    public class CompProximityFuseWithCount : ThingComp
     {
-        if (Find.TickManager.TicksGame % 250 == 0)
-        {
-            CompTickRare();
-        }
-    }
+        public CompProperties_ProximityFuseWithCount Props => (CompProperties_ProximityFuseWithCount)props;
 
-    public override IEnumerable<Gizmo> CompGetGizmosExtra()
-    {
-        if (DebugSettings.ShowDevGizmos)
+        public override void CompTick()
         {
-            yield return new Command_Action
+            if (Find.TickManager.TicksGame % 250 == 0)
             {
-                defaultLabel = "DEV: Trigger",
-                action = delegate
-                {
-                    parent.GetComp<CompExplosive>().StartWick();
-                }
-            };
+                CompTickRare();
+            }
         }
-    }
 
-    public override void CompTickRare()
-    {
-        Pawn pawn = parent as Pawn;
-        if (parent.Spawned && !pawn.Dead)
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
         {
-            int count = 0;
-            foreach (var cell in GenRadial.RadialCellsAround(parent.Position, Props.radius, true))
+            if (DebugSettings.ShowDevGizmos)
             {
-                if (!cell.InBounds(parent.Map)) continue;
-
-                List<Thing> things = parent.Map.thingGrid.ThingsListAtFast(cell);
-                for (int i = 0; i < things.Count; i++)
+                yield return new Command_Action
                 {
-                    if (things[i].def == Props.target)
+                    defaultLabel = "DEV: Trigger",
+                    action = delegate
                     {
-                        count++;
-                        break;
+                        parent.GetComp<CompExplosive>().StartWick();
+                    }
+                };
+            }
+        }
+
+        public override void CompTickRare()
+        {
+            Pawn pawn = parent as Pawn;
+            if (parent.Spawned && !pawn.Dead)
+            {
+                int count = 0;
+                foreach (var cell in GenRadial.RadialCellsAround(parent.Position, Props.radius, true))
+                {
+                    if (!cell.InBounds(parent.Map)) continue;
+
+                    List<Thing> things = parent.Map.thingGrid.ThingsListAtFast(cell);
+                    for (int i = 0; i < things.Count; i++)
+                    {
+                        if (things[i].def == Props.target)
+                        {
+                            count++;
+                            break;
+                        }
                     }
                 }
-            }
 
-            if (count >= Props.amount || pawn.IsBurning()) {
-                parent.GetComp<CompExplosive>().StartWick();
+                if (count >= Props.amount || pawn.IsBurning())
+                {
+                    parent.GetComp<CompExplosive>().StartWick();
 
+                }
             }
         }
     }
---
+++ 1.6/Source/DeathActionProperties/Properties/DeathActionProperties_DroneDivide.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Collections.Generic;
+using Verse;
+namespace VanillaQuestsExpandedDroneFactory
+
+{
+    public class DeathActionProperties_DroneDivide : DeathActionProperties
+    {
+        public List<PawnKindDef> dividePawnKindOptions = new List<PawnKindDef>();
+
+        public DeathActionProperties_DroneDivide()
+        {
+            workerClass = typeof(DeathActionWorker_DroneDivide);
+        }
+
+      
+    }
+}
