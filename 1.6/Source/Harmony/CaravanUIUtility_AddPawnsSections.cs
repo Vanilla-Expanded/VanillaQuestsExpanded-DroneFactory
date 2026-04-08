5a1dd1f4ee33b3a50bef80988c8d671cb37df585
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-04-08 14:49:03 +0200

    Pack drone tweaks

---
+++ 1.6/Source/Harmony/CaravanUIUtility_AddPawnsSections.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using RimWorld.Planet;
+using RimWorld;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using Verse;
+
+namespace VanillaQuestsExpandedDroneFactory
+{
+    [HarmonyPatch]
+    public static class VanillaQuestsExpandedDroneFactory_CaravanUIUtility_AddPawnsSections_Patch
+    {
+        public static MethodBase TargetMethod()
+        {
+            foreach (var nestedType in typeof(CaravanUIUtility).GetNestedTypes(AccessTools.all))
+            {
+                var methods = nestedType.GetMethods(AccessTools.all);
+                var method = methods.FirstOrDefault(x => x.Name.Contains("<AddPawnsSections>b__8_7"));
+                if (method != null)
+                {
+                    return method;
+                }
+            }
+            return null;
+        }
+
+        public static void Postfix(TransferableOneWay x, ref bool __result)
+        {
+            if (!__result && x.AnyThing is Pawn pawn && StaticCollections.pawnCapacityLabels.ContainsKey(pawn.def))
+            {
+                __result = true;
+            }
+        }
+    }
+}
---
+++ 1.6/Source/PawnRenderNodeWorkers/PawnRenderNodeWorker_DronePack.cs
@@ -0,0 +1,16 @@
+﻿
+using Verse;
+namespace VanillaQuestsExpandedDroneFactory
+{
+    public class PawnRenderNodeWorker_DronePack : PawnRenderNodeWorker
+    {
+        public override bool CanDrawNow(PawnRenderNode node, PawnDrawParms parms)
+        {
+            if (base.CanDrawNow(node, parms) && !parms.Portrait && StaticCollections.pawnCapacityLabels.ContainsKey(parms.pawn.def) && parms.pawn.inventory != null)
+            {
+                return parms.pawn.inventory.innerContainer.Count > 0;
+            }
+            return false;
+        }
+    }
+}
