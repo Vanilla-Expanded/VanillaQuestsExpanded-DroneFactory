565bd1f7dd5628cbe9a41b6efeeaf243e0c6a1bc
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-04-11 22:52:02 +0200

    Factory done

---
+++ 1.6/Source/DefExtensions/ProcessDefExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace VanillaQuestsExpandedDroneFactory
+{
+
+    public class ProcessDefExtension : DefModExtension
+    {
+
+        public RecipeDef associatedLockableRecipe = null;
+     
+    }
+
+
+}
---
+++ 1.6/Source/Harmony/ProcessDef_ShowProcess.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+using PipeSystem;
+
+namespace VanillaQuestsExpandedDroneFactory
+{
+    [HarmonyPatch(typeof(ProcessDef), nameof(ProcessDef.ShowProcess))]
+    public static class VanillaQuestsExpandedDroneFactory_ProcessDef_ShowProcess_Patch
+    {
+        private static void Postfix(ProcessDef __instance, ref bool __result)
+        {
+            if (__instance.GetModExtension<ProcessDefExtension>() != null)
+            {
+                ProcessDefExtension extension = __instance.GetModExtension<ProcessDefExtension>();
+                if (WorldComponent_UnlockedRecipes.Instance.unlocked_Recipes.ContainsKey(extension.associatedLockableRecipe))
+                {
+                    if (!WorldComponent_UnlockedRecipes.Instance.unlocked_Recipes[extension.associatedLockableRecipe])
+                    {
+                        __result = false;
+                    }
+                }
+
+            }
+            
+        }
+    }
+}
